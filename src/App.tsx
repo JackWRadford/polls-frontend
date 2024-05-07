f22acabb5889da05f92e1e261890658a7e830a60
Jack Radford <jack.radford@live.co.uk>
2024-05-07 11:56:58 +0100

    Improved the Button component. Added clsx. Added css color vars.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Button from "./components/button/Button";
 
 function App() {
-	return <div></div>;
+	return (
+		<>
+			<Button
+				label={"Hold to Create"}
+				onClick={function (): void {
+					throw new Error("Function not implemented.");
+				}}
+			></Button>
+			<Button
+				label={"Hold to Create"}
+				onClick={function (): void {
+					throw new Error("Function not implemented.");
+				}}
+				type="secondary"
+			></Button>
+		</>
+	);
 }
 
 export default App;
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,14 +1,20 @@
-import { MouseEvent } from "react";
+import clsx from "clsx";
 import styles from "./button.module.css";
 
 interface ButtonProps {
 	label: string;
-	onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+	onClick: () => void;
+	type?: "primary" | "secondary";
 }
 
-const Button = ({ label, onClick }: ButtonProps) => {
+const Button = ({ label, onClick, type = "primary" }: ButtonProps) => {
+	const secondaryClass = type == "secondary" ? styles.secondary : null;
+
 	return (
-		<button className={styles.button} onClick={onClick}>
+		<button
+			className={clsx(styles.button, secondaryClass)}
+			onClick={onClick}
+		>
 			{label}
 		</button>
 	);
