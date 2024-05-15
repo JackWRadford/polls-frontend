0296bcbb0844051548c4dcd433aff39b70d85312
Jack Radford <jack.radford@live.co.uk>
2024-05-15 14:51:17 +0100

    Added the ProgressBar to the PollResultsCard and improved the styling.

--- src/components/poll-results-card/PollResultsCard.tsx
+++ src/components/poll-results-card/PollResultsCard.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import Card from "../common/card/Card";
 import { Poll } from "../../models/poll";
 import styles from "./pollResultsCard.module.css";
+import ProgressBar from "../common/progress-bar/ProgressBar";
 
 interface PollResultsCardProps {
 	pollId: string;
@@ -47,9 +48,13 @@ const PollResultsCard = ({ pollId }: PollResultsCardProps) => {
 	}, [pollId]);
 
 	const voteCountLabel: string = useMemo(
-		() => ((pollResults?.totalVoteCount ?? 0) > 1 ? "Votes" : "Vote"),
+		() => getVoteCountLabel(pollResults?.totalVoteCount ?? 0),
 		[pollResults?.totalVoteCount]
 	);
+
+	function getVoteCountLabel(voteCount: number): string {
+		return voteCount > 1 || voteCount === 0 ? "Votes" : "Vote";
+	}
 
 	return (
 		<Card className={styles.container}>
@@ -57,8 +62,15 @@ const PollResultsCard = ({ pollId }: PollResultsCardProps) => {
 			<ul>
 				{pollResults?.pollResultsData.map((optionResultData, index) => (
 					<li key={index}>
-						<p>{optionResultData.optionTitle}</p>
-						<p>{`${optionResultData.percentage}% (${optionResultData.count})`}</p>
+						<div className={styles.itemLabels}>
+							<p>{optionResultData.optionTitle}</p>
+							<p>{`${optionResultData.percentage}% (${
+								optionResultData.count
+							} ${getVoteCountLabel(
+								optionResultData.count
+							)})`}</p>
+						</div>
+						<ProgressBar value={optionResultData.percentage} />
 					</li>
 				))}
 			</ul>
