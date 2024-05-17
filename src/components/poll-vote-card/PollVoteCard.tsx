3f952b1b5540e17a4c843d70cdffb7b68f726762
Jack Radford <jack.radford@live.co.uk>
2024-05-17 22:10:37 +0100

    Added a poll prop to the PollVoteCard and moved the poll fetch request to the PollVotePage.

--- src/components/poll-vote-card/PollVoteCard.tsx
+++ src/components/poll-vote-card/PollVoteCard.tsx
@@ -1,39 +1,17 @@
-import { useParams } from "react-router-dom";
 import Card from "../common/card/Card";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import styles from "./pollVoteCard.module.css";
 import RadioSelect from "../common/radio-select/RadioSelect";
 import Button from "../common/button/Button";
-import { Poll } from "../../models/poll";
 import LinkButton from "../common/link/LinkButton";
+import { Poll } from "../../models/poll";
 
-const PollVoteCard = () => {
-	const { id } = useParams();
-	const [poll, setPoll] = useState<Poll>();
+interface PollVoteCardProps {
+	poll?: Poll;
+}
+
+const PollVoteCard = ({ poll }: PollVoteCardProps) => {
 	const [selectedOptionId, setSelectedOptionId] = useState<string>();
-
-	useEffect(() => {
-		const fetchPoll = async () => {
-			try {
-				const result = await fetch(
-					`http://localhost:3000/polls/${id}`,
-					{
-						method: "GET",
-					}
-				);
-				if (!result.ok) {
-					console.error("Error while fetching poll.");
-				} else {
-					const data = await result.json();
-					setPoll(data as Poll);
-				}
-			} catch (error) {
-				console.error("Error while fetching poll.", error);
-			}
-		};
-
-		fetchPoll();
-	}, [id]);
 
 	const submitIsDisabled = useMemo((): boolean => {
 		return selectedOptionId === undefined;
--- src/pages/poll-vote-page/PollVotePage.tsx
+++ src/pages/poll-vote-page/PollVotePage.tsx
@@ -1,10 +1,39 @@
+import { useParams } from "react-router-dom";
 import PollVoteCard from "../../components/poll-vote-card/PollVoteCard";
 import styles from "./pollVotePage.module.css";
+import { useEffect, useState } from "react";
+import { Poll } from "../../models/poll";
 
 const PollVotePage = () => {
+	const { id } = useParams();
+	const [poll, setPoll] = useState<Poll>();
+
+	useEffect(() => {
+		const fetchPoll = async () => {
+			try {
+				const result = await fetch(
+					`http://localhost:3000/polls/${id}`,
+					{
+						method: "GET",
+					}
+				);
+				if (!result.ok) {
+					console.error("Error while fetching poll.");
+				} else {
+					const data = await result.json();
+					setPoll(data as Poll);
+				}
+			} catch (error) {
+				console.error("Error while fetching poll.", error);
+			}
+		};
+
+		fetchPoll();
+	}, [id]);
+
 	return (
 		<div className={styles.container}>
-			<PollVoteCard />
+			<PollVoteCard poll={poll} />
 		</div>
 	);
 };
