160aa60abdd335ed6b0a4da68789cd157e16dcb5
Jack Radford <jack.radford@live.co.uk>
2024-05-17 22:44:39 +0100

    Implemented the recent polls list in RecentPolls. Moved the PollVoteCard top margin from the card to the page component.

--- src/components/home/recent-polls/RecentPolls.tsx
+++ src/components/home/recent-polls/RecentPolls.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import styles from "./recentPolls.module.css";
 import { Poll } from "../../../models/poll";
+import PollVoteCard from "../../poll-vote-card/PollVoteCard";
+import Button from "../../common/button/Button";
+
+type PollsResponse = {
+	polls: Poll[];
+	thereAreMorePolls: boolean;
+};
 
 const RecentPolls = () => {
 	const [page, setPage] = useState(1);
 	const [polls, setPolls] = useState<Poll[]>();
+	const [thereAreMorePolls, setThereAreMorePolls] = useState(false);
 
 	useEffect(() => {
 		const fetchPolls = async () => {
@@ -18,9 +26,18 @@ const RecentPolls = () => {
 				if (!response.ok) {
 					throw new Error();
 				} else {
-					const data = await response.json();
-					console.log(data);
-					setPolls(data as Poll[]);
+					const data = (await response.json()) as PollsResponse;
+
+					// Update the polls array. Adding the new polls if the page is not 1.
+					setPolls((existingPolls) => {
+						if (page === 1) {
+							return data.polls;
+						} else {
+							return [...(existingPolls || []), ...data.polls];
+						}
+					});
+
+					setThereAreMorePolls(data.thereAreMorePolls);
 				}
 			} catch (error) {
 				console.error("Error while fetching polls: ", error);
@@ -30,7 +47,26 @@ const RecentPolls = () => {
 		fetchPolls();
 	}, [page]);
 
-	return <div className={styles.container}></div>;
+	const handleMorePolls = () => {
+		setPage((prev) => (prev += 1));
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.polls}>
+				{polls?.map((poll) => (
+					<PollVoteCard key={poll._id} poll={poll} />
+				))}
+			</div>
+			{(thereAreMorePolls && (
+				<Button label="More Polls" onClick={handleMorePolls} />
+			)) || (
+				<p className={styles.noMorePolls}>
+					That's all the polls we have.
+				</p>
+			)}
+		</div>
+	);
 };
 
 export default RecentPolls;
