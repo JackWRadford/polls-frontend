125bfe676ece88d8c68b49b928855d1f0fcbbc60
Jack Radford <jack.radford@live.co.uk>
2024-06-14 18:37:44 +0100

    Added TitleSubtitle to the CreatePollPage. Added an optional className to the TitleSubtitle component.

--- src/components/common/title-subtitle/TitleSubtitle.tsx
+++ src/components/common/title-subtitle/TitleSubtitle.tsx
@@ -1,20 +1,23 @@
+import clsx from "clsx";
 import styles from "./titleSubtitle.module.css";
 
 export interface TitleSubtitleProps {
 	title: string;
 	subtitle?: string;
 	titleElementType?: "h1" | "h2" | "h3";
+	className?: string;
 }
 
 const TitleSubtitle = ({
 	title,
 	subtitle,
 	titleElementType = "h2",
+	className,
 }: TitleSubtitleProps) => {
 	const TitleElement = titleElementType || "h2";
 
 	return (
-		<div className={styles.container}>
+		<div className={clsx(className, styles.container)}>
 			<TitleElement>{title}</TitleElement>
 			<p className={styles.subtitle}>{subtitle}</p>
 		</div>
--- src/pages/create-poll-page/CreatePollPage.tsx
+++ src/pages/create-poll-page/CreatePollPage.tsx
@@ -1,3 +1,4 @@
+import TitleSubtitle from "../../components/common/title-subtitle/TitleSubtitle";
 import CreatePollCard from "../../components/create-poll-card/CreatePollCard";
 import styles from "./createPollPage.module.css";
 import { useEffect } from "react";
@@ -9,6 +10,12 @@ const CreatePollPage = () => {
 
 	return (
 		<div className={styles.container}>
+			<TitleSubtitle
+				title="Create a poll now!"
+				subtitle="Design engaging questions and collect valuable insights"
+				titleElementType="h1"
+				className={styles.title}
+			/>
 			<CreatePollCard />
 		</div>
 	);
