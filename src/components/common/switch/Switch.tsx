eb16326d12a9984cbba9ce46321eacc811e3015c
Jack Radford <jack.radford@live.co.uk>
2024-06-20 17:41:00 +0100

    Added a Switch checkbox input component.

---
+++ src/components/common/switch/Switch.tsx
@@ -0,0 +1,29 @@
+import styles from "./switch.module.css";
+
+interface SwitchProps {
+	id: string;
+	label?: string;
+	checked: boolean;
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Switch = ({ id, label, checked, onChange }: SwitchProps) => {
+	return (
+		<div className={styles.switchContainer}>
+			{label && (
+				<label htmlFor={id} className={styles.label}>
+					{label}
+				</label>
+			)}
+			<input
+				id={id}
+				type="checkbox"
+				className={styles.input}
+				checked={checked}
+				onChange={onChange}
+			/>
+		</div>
+	);
+};
+
+export default Switch;
